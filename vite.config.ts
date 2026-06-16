9c556e61b40b6a6de64017ecf8dbc89cbbbe395e
camaheepkohli <camaheepkohli@gmail.com>
2026-06-16 23:27:51 +0530

    fix: resolve vite path alias mapping

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import netlify from '@netlify/vite-plugin-tanstack-start'; // <-- Add this import
+import netlify from '@netlify/vite-plugin-tanstack-start';
+import tsconfigPaths from 'vite-tsconfig-paths'; // 1. Add this import
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(), // 2. Add this right here at the top!
     tanstackStart(), 
-    netlify() // <-- Add this function call inside the plugins array
+    netlify()
   ],
 });
