43c0e3675fad9ed8f5f8f6afc45e8f8c48d58cd8
camaheepkohli <camaheepkohli@gmail.com>
2026-06-16 23:42:11 +0530

    fix: override routing logic for lovable tanstack start app

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import netlify from '@netlify/vite-plugin-tanstack-start';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
-    tanstackStart(), 
-    netlify()
+    tanstackStart(),
   ],
 });
